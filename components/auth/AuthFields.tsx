61ae62d981b6137a33fff611cbfc1fc5d711da69
Linnea Blad <bladlinnea@gmail.com>
2022-05-03 13:23:06 +0100

    Added email and password validation for register-component

--- components/auth/AuthFields.tsx
+++ components/auth/AuthFields.tsx
@@ -1,7 +1,33 @@
 import { View, Text, TextInput, Button } from "react-native";
+import { showMessage } from "react-native-flash-message";
 import { Typography, Forms, Base } from '../../styles';
 
 export default function AuthFields({ auth, setAuth, title, submit, navigation}) {
+
+    function validateEmail(text: string) {
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!text.match(pattern)) {
+            showMessage({
+                message: "Icke giltig email",
+                description: "Email måste uppfylla typ cccc@cc.cc",
+                type: "warning",
+                statusBarHeight: 20,
+            });
+        }
+    }
+
+    function validatePassword(text: string) {
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\.-]).{4,}$/
+        if (!text.match(pattern)) {
+            showMessage({
+                message: "Icke giltigt lösenord",
+                description: "Lösenordet måste innehålla minst 4 tecken, små och stora bokstäver, siffror och specialtecken",
+                type: "warning",
+                statusBarHeight: 20,
+            });
+        }
+    }
+
     return (
         <View style={Base.base}>
             <Text style={Typography.header2}>{title}</Text>
@@ -9,7 +35,11 @@ export default function AuthFields({ auth, setAuth, title, submit, navigation}) 
             <TextInput
                 style={Forms.input}
                 onChangeText={(content: string) => {
+                    if (title === "Registrera") {
+                        validateEmail(content);
+                    }
                     setAuth({ ...auth, email: content })
+
                 }}
                 value={auth?.email}
                 keyboardType="email-address"
@@ -19,7 +49,11 @@ export default function AuthFields({ auth, setAuth, title, submit, navigation}) 
             <TextInput
                 style={Forms.input}
                 onChangeText={(content: string) => {
+                    if (title === "Registrera") {
+                        validatePassword(content);
+                    }
                     setAuth({ ...auth, password: content })
+
                 }}
                 value={auth?.password}
                 secureTextEntry={true}
--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -6,9 +6,11 @@ import AuthFields from './AuthFields';
 
 export default function Register({navigation, setIsLoggedIn}) {
     const [auth, setAuth] = useState<Partial<Auth>>({});
+    const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\.-]).{4,}$/
 
     async function doRegister() {
-        if (auth.email && auth.password) {
+        if (auth?.email?.match(emailPattern) && auth?.password?.match(passwordPattern)) {
             const result = await authModel.register(auth.email, auth.password);
 
             showMessage({
@@ -24,8 +26,8 @@ export default function Register({navigation, setIsLoggedIn}) {
 
         } else {
             showMessage({
-                message: "Saknas",
-                description: "E-post eller lösenord saknas",
+                message: "Icke giltig email eller lösenord",
+                description: "Skriv in en giltig email och lösenord",
                 type: "warning",
                 statusBarHeight: 20,
             });
